6036dee1e15caa415f1da9ab41e5ade2c81df1c0
Diego-jpeg-27 <diego.developTI@gmail.com>
2025-09-24 19:38:12 -0300

    fix: use nullable() for birth_date in authors migration
    Corrige erro na migration create_authors_table: substitui ->NULL (método inexistente) por ->nullable(); para permitir valores nulos em birth_date.

--- atividade-01-migrations-laravel/database/migrations/2025_09_23_124533_create_authors_table.php
+++ atividade-01-migrations-laravel/database/migrations/2025_09_23_124533_create_authors_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->bigincrements('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
-            $table->date('birth_date')->NULL;
+            $table->date('birth_date')->nullable();
             $table->timestamps();
         });
     }
