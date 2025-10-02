3de68c9bb693f6ab63881fc90bc74b2d064cd541
Diego-jpeg-27 <diego.developTI@gmail.com>
2025-10-02 00:23:27 -0300

    refactor(atividade-02): update create view to use partial form
    Refatoração da view create.blade.php para utilizar o partial _form.blade.php, promovendo reutilização de código e mantendo o padrão de organização para futuros usos, como no edit.blade.php.

--- atividade-02-crud-laravel/resources/views/animes/create.blade.php
+++ atividade-02-crud-laravel/resources/views/animes/create.blade.php
@@ -1,41 +1,7 @@
-<html>
-<head>
- 
-</head>
-<body>
-    <div class="container mt-4">
-        <h1 class="mb-4">Cadastrar Novo Anime</h1>
+@extends('layouts.app')
 
-        <form action="{{ route('animes.store') }}" method="POST">
-            @csrf
+@section('content')
+    <h1 class="mb-4">Cadastrar Novo Anime</h1>
 
-            <div class="mb-3">
-                <label for="title" class="form-label">Título</label>
-                <input type="text" id="title" name="title" class="form-control" required>
-            </div>
-
-            <div class="mb-3">
-                <label for="description" class="form-label">Descrição</label>
-                <textarea id="description" name="description" class="form-control" required></textarea>
-            </div>
-
-            <div class="mb-3">
-                <label for="genre" class="form-label">Gênero</label>
-                <input type="text" id="genre" name="genre" class="form-control" required>
-            </div>
-
-            <div class="mb-3">
-                <label for="creator" class="form-label">Criador</label>
-                <input type="text" id="creator" name="creator" class="form-control" required>
-            </div>
-
-            <div class="mb-3">
-                <label for="release_year" class="form-label">Ano de Lançamento</label>
-                <input type="number" id="release_year" name="release_year" class="form-control" required>
-            </div>
-
-            <button type="submit" class="btn btn-success">Salvar</button>
-        </form>
-    </div>
-</body>
-</html>
+    @include('animes._form', ['anime' => null])
+@endsection
