5aea40205a9f8529a256553f490e43f3b5abe55f
Diego-jpeg-27 <diego.developTI@gmail.com>
2025-10-03 10:40:11 -0300

    feat(atividade-02): create main stylesheet in app.css
    Criado o arquivo app.css com os estilos principais da tela principal, incluindo layout centralizado, botões e tipografia. ajuste configuração do vite.config.js

--- atividade-02-crud-laravel/vite.config.js
+++ atividade-02-crud-laravel/vite.config.js
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            //add TODOS os arquivos CSS/JS que serão usados pelo Vite
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',    
+            ],
             refresh: true,
         }),
         tailwindcss(),
