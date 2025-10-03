e085da7d570b7dc52e392cdc8727059f9c658e8d
Diego-jpeg-27 <diego.developTI@gmail.com>
2025-10-03 11:16:29 -0300

    feat(atividade-02): define web routes for Anime CRUD and home
    Adicionadas e atualizadas as rotas da aplicação para o CRUD de animes, bem como a rota principal para a página de boas-vindas. Usado padrão RESTful e rotas nomeadas.

--- atividade-02-crud-laravel/routes/web.php
+++ atividade-02-crud-laravel/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnimeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/animes-lista', [AnimeController::class, 'lista'])->name('animes.lista');
+Route::resource('animes', AnimeController::class);
