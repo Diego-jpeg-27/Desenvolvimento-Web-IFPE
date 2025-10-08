6872f618f82a2357b3b83e7d73d33d37c0b87c94
Diego-jpeg-27 <diego.developTI@gmail.com>
2025-10-08 14:11:32 -0300

    feat(atividade-03): configure relationship between Author and Book models
    [ x ] Relacionamento hasMany adicionado ao modelo Author
    [ x ] Uso do trait HasFactory mantido para uso futuro de factories
    [ x ] Testado carregamento do relacionamento com o método books()

--- atividade-03-models-seeds-factories/app/Models/Author.php
+++ atividade-03-models-seeds-factories/app/Models/Author.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+//Adicionado o trait HasFactory, Mesmo que as factories ainda não tenham sido criadas, o uso antecipado de HasFactory prepara o modelo.
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'birth_date', 'email'];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
