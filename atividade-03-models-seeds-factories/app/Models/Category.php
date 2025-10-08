f3f7c6ec0a22e2fe964ac578728575c32fd8c8c5
Diego-jpeg-27 <diego.developTI@gmail.com>
2025-10-08 13:57:50 -0300

    feat(atividade-03): configure relationship between Category and Book models
    Adicionado relacionamento hasMany no modelo Category para vincular múltiplos registros de Book.
    Também foi adicionado o trait HasFactory, utilizado pelo Laravel para permitir o uso de model factories.
    Mesmo que as factories ainda não tenham sido criadas, o uso antecipado de HasFactory prepara o modelo
    para as próximas etapas da atividade, onde serão utilizados seeders e dados fake.

--- atividade-03-models-seeds-factories/app/Models/Category.php
+++ atividade-03-models-seeds-factories/app/Models/Category.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+//Adicionado o trait HasFactory, Mesmo que as factories ainda não tenham sido criadas, o uso antecipado de HasFactory prepara o modelo.
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
