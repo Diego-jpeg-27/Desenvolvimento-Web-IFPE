0b2db0fc574d574bcaa0caec0614352dd84453d0
Diego-jpeg-27 <diego.developTI@gmail.com>
2025-10-08 18:03:15 -0300

    feat(atividade-03): add fake company name and address to PublisherFactory
    PublisherFactory agora gera automaticamente nome de empresa único e endereço completo usando Faker.
    Pronta para popular a tabela de editoras com dados realistas.

--- atividade-03-models-seeds-factories/database/factories/PublisherFactory.php
+++ atividade-03-models-seeds-factories/database/factories/PublisherFactory.php
@@ -2,22 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\Publisher;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Publisher>
- */
 class PublisherFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Publisher::class;
+
+    public function definition()
     {
         return [
-            //
+            'name' => $this->faker->unique()->company, // Gera um nome de empresa único
+            'address' => $this->faker->address,
         ];
     }
 }
