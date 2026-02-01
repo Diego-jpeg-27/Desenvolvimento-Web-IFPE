f67b1f2de8336f058fa03217706fa7fc515c4c9f
Diego-jpeg-27 <diego.developTI@gmail.com>
2026-02-01 15:13:11 -0300

    feat(atividade-12): prepare seeders and factories with integrated fake data
    Preparação de seeders e factories com dados falsos integrados para as tabelas de autores, categorias, editoras e livros. Inclui a criação de seeders específicos, factories com relações e validação no banco de dados.

--- atividade-12-api-book-crud/database/seeders/DatabaseSeeder.php
+++ atividade-12-api-book-crud/database/seeders/DatabaseSeeder.php
@@ -2,24 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as FakerFactory;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        FakerFactory::create()->unique(true);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            CategorySeeder::class,
+            AuthorPublisherBookSeeder::class,
         ]);
     }
 }
